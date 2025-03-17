08aae569f90b5c63d266c9b22d1425dd9451a0ae
DuyVoK15 <duybpz@gmail.com>
2025-03-17 08:23:14 +0700

    config: vite alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src/"),
+      components: `${path.resolve(__dirname, "./src/components/")}`,
+      public: `${path.resolve(__dirname, "./public/")}`,
+      pages: path.resolve(__dirname, "./src/pages"),
+      types: `${path.resolve(__dirname, "./src/@types")}`,
+    },
+  },
 });
